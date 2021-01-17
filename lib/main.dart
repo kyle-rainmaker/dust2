95cbd9ec4fd5f3cadf0f569e2544d2717433c956
kyle-rainmaker <nusider@gmail.com>
2021-01-17 16:56:49 +0900

    색깔 수정

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo 진짠가?',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+
       ),
       home: Main(),
     );
